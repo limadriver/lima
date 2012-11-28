5b864a6ef670bb913d06a13c5b9c2ff89e7950c1
Luc Verhaegen <libv@skynet.be>
2012-11-28 18:46:01 +0100

    limare:tests:cube: attribute Arvin Schnell for his smoothed cube
    Signed-off-by: Luc Verhaegen <libv@skynet.be>

--- limare/tests/cube/egl.c
+++ limare/tests/cube/egl.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011-2012 Luc Verhaegen <libv@skynet.be>
+ * Copyright (c) 2012 Arvin Schnell <arvin.schnell@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
