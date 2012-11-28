5cfd21ab3f6d987f1e70c4940c3ff171619e9100
Luc Verhaegen <libv@skynet.be>
2012-11-28 19:11:40 +0100

    limare:tests: add cube companion test
    Signed-off-by: Luc Verhaegen <libv@skynet.be>

---
+++ limare/tests/cube_companion/egl.c
@@ -0,0 +1,385 @@
+/*
+ * Copyright (c) 2011-2012 Luc Verhaegen <libv@skynet.be>
+ * Copyright (c) 2012 Arvin Schnell <arvin.schnell@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sub license,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+#include "esUtil.h"
+#include "companion.h"
+
+#define ONSCREEN 0
+#define WIDTH  800
+#define HEIGHT 480
+
+static EGLint const config_attribute_list[] = {
+	EGL_RED_SIZE, 8,
+	EGL_GREEN_SIZE, 8,
+	EGL_BLUE_SIZE, 8,
+	EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+	EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+	EGL_DEPTH_SIZE, 8,
+	EGL_NONE
+};
+
+static EGLint const pbuffer_attribute_list[] = {
+	EGL_WIDTH, WIDTH,
+	EGL_HEIGHT, HEIGHT,
+	EGL_LARGEST_PBUFFER, EGL_TRUE,
+	EGL_NONE
+};
+
+static const EGLint context_attribute_list[] = {
+	EGL_CONTEXT_CLIENT_VERSION, 2,
+	EGL_NONE
+};
+
+char *
+eglStrError(EGLint error)
+{
+	printf("Error: %d\n", error);
+
+	switch (error) {
+	case EGL_SUCCESS:
+		return "EGL_SUCCESS";
+	case EGL_BAD_ALLOC:
+		return "EGL_BAD_ALLOC";
+	case EGL_BAD_CONFIG:
+		return "EGL_BAD_CONFIG";
+	case EGL_BAD_PARAMETER:
+		return "EGL_BAD_PARAMETER";
+	case EGL_BAD_MATCH:
+		return "EGL_BAD_MATCH";
+	case EGL_BAD_ATTRIBUTE:
+		return "EGL_BAD_ATTRIBUTE";
+	default:
+		return "UNKNOWN";
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	EGLDisplay display;
+	EGLint egl_major, egl_minor;
+	EGLConfig config;
+	EGLint num_config;
+	EGLContext context;
+	EGLSurface surface;
+	GLuint vertex_shader;
+	GLuint fragment_shader;
+	GLuint program;
+	GLint ret;
+	GLint width, height;
+	GLuint texture;
+
+#if ONSCREEN
+	struct mali_native_window window = {WIDTH, HEIGHT};
+#endif
+
+	const char *vertex_shader_source =
+	  "uniform mat4 modelviewMatrix;\n"
+	  "uniform mat4 modelviewprojectionMatrix;\n"
+	  "uniform mat3 normalMatrix;\n"
+	  "\n"
+	  "attribute vec4 in_position;    \n"
+	  "attribute vec3 in_normal;      \n"
+	  "attribute vec2 in_coord;       \n"
+	  "\n"
+	  "vec4 lightSource = vec4(2.0, 2.0, 20.0, 0.0);\n"
+	  "                             \n"
+	  "varying vec4 vVaryingColor;  \n"
+	  "varying vec2 coord;          \n"
+	  "                             \n"
+	  "void main()                  \n"
+	  "{                            \n"
+	  "    gl_Position = modelviewprojectionMatrix * in_position;\n"
+	  "    vec3 vEyeNormal = normalMatrix * in_normal;\n"
+	  "    vec4 vPosition4 = modelviewMatrix * in_position;\n"
+	  "    vec3 vPosition3 = vPosition4.xyz / vPosition4.w;\n"
+	  "    vec3 vLightDir = normalize(lightSource.xyz - vPosition3);\n"
+	  "    float diff = max(0.0, dot(vEyeNormal, vLightDir));\n"
+	  "    vVaryingColor = vec4(diff * vec3(1.0, 1.0, 1.0), 1.0);\n"
+	  "    coord = in_coord;        \n"
+	  "}                            \n";
+
+	const char *fragment_shader_source =
+	  "precision mediump float;     \n"
+	  "                             \n"
+	  "varying vec4 vVaryingColor;  \n"
+	  "varying vec2 coord;          \n"
+	  "                             \n"
+	  "uniform sampler2D in_texture; \n"
+	  "                             \n"
+	  "void main()                  \n"
+	  "{                            \n"
+	  "    gl_FragColor = 3.0 * vVaryingColor * texture2D(in_texture, coord);\n"
+	  "}                            \n";
+
+	float *vertices_array = companion_vertices_array();
+	float *texture_coordinates_array =
+		companion_texture_coordinates_array();
+	float *normals_array = companion_normals_array();
+
+	display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+	if (display == EGL_NO_DISPLAY) {
+		printf("Error: No display found!\n");
+		return -1;
+	}
+
+	if (!eglInitialize(display, &egl_major, &egl_minor)) {
+		printf("Error: eglInitialise failed!\n");
+		return -1;
+	}
+
+	printf("Using display %p with EGL version %d.%d\n",
+	       display, egl_major, egl_minor);
+
+	printf("EGL Version \"%s\"\n", eglQueryString(display, EGL_VERSION));
+	printf("EGL Vendor \"%s\"\n", eglQueryString(display, EGL_VENDOR));
+	printf("EGL Extensions \"%s\"\n", eglQueryString(display, EGL_EXTENSIONS));
+
+	/* get an appropriate EGL frame buffer configuration */
+	eglChooseConfig(display, config_attribute_list, &config, 1, &num_config);
+
+	/* create an EGL rendering context */
+	context = eglCreateContext(display, config, EGL_NO_CONTEXT, context_attribute_list);
+	if (context == EGL_NO_CONTEXT) {
+		printf("Error: eglCreateContext failed: %d\n", eglGetError());
+		return -1;
+	}
+
+#if ONSCREEN
+	surface = eglCreateWindowSurface(display, config, &window, NULL);
+	if (surface == EGL_NO_SURFACE) {
+		printf("Error: eglCreateWindowSurface failed: %d (%s)\n",
+		       eglGetError(), eglStrError(eglGetError()));
+		return -1;
+	}
+#else
+	surface = eglCreatePbufferSurface(display, config, pbuffer_attribute_list);
+	if (surface == EGL_NO_SURFACE) {
+		printf("Error: eglCreatePbufferSurface failed: %d (%s)\n",
+		       eglGetError(), eglStrError(eglGetError()));
+		return -1;
+	}
+#endif
+
+	if (!eglQuerySurface(display, surface, EGL_WIDTH, &width) ||
+	    !eglQuerySurface(display, surface, EGL_HEIGHT, &height)) {
+		printf("Error: eglQuerySurface failed: %d (%s)\n",
+		       eglGetError(), eglStrError(eglGetError()));
+		return -1;
+	}
+	printf("PBuffer: %dx%d\n", width, height);
+
+	/* connect the context to the surface */
+	if (!eglMakeCurrent(display, surface, surface, context)) {
+		printf("Error: eglMakeCurrent() failed: %d (%s)\n",
+		       eglGetError(), eglStrError(eglGetError()));
+		return -1;
+	}
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	if (!vertex_shader) {
+		printf("Error: glCreateShader(GL_VERTEX_SHADER) failed: %d (%s)\n",
+		       eglGetError(), eglStrError(eglGetError()));
+		return -1;
+	}
+
+
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
+
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &ret);
+	if (!ret) {
+		char *log;
+
+		printf("Error: vertex shader compilation failed!:\n");
+		glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &ret);
+
+		if (ret > 1) {
+			log = malloc(ret);
+			glGetShaderInfoLog(vertex_shader, ret, NULL, log);
+			printf("%s", log);
+		}
+		return -1;
+	} else
+		printf("Vertex shader compilation succeeded!\n");
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	if (!fragment_shader) {
+		printf("Error: glCreateShader(GL_FRAGMENT_SHADER) failed: %d (%s)\n",
+		       eglGetError(), eglStrError(eglGetError()));
+		return -1;
+	}
+
+
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
+
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &ret);
+	if (!ret) {
+		char *log;
+
+		printf("Error: fragment shader compilation failed!:\n");
+		glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &ret);
+
+		if (ret > 1) {
+			log = malloc(ret);
+			glGetShaderInfoLog(fragment_shader, ret, NULL, log);
+			printf("%s", log);
+		}
+		return -1;
+	} else
+		printf("Fragment shader compilation succeeded!\n");
+
+	program = glCreateProgram();
+	if (!program) {
+		printf("Error: failed to create program!\n");
+		return -1;
+	}
+
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+
+	glBindAttribLocation(program, 0, "in_position");
+	glBindAttribLocation(program, 1, "in_normal");
+	glBindAttribLocation(program, 2, "in_coord");
+
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &ret);
+	if (!ret) {
+		char *log;
+
+		printf("Error: program linking failed!:\n");
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &ret);
+
+		if (ret > 1) {
+			log = malloc(ret);
+			glGetProgramInfoLog(program, ret, NULL, log);
+			printf("%s", log);
+		}
+		return -1;
+	} else
+		printf("program linking succeeded!\n");
+
+	glUseProgram(program);
+
+	glViewport(0, 0, width, height);
+
+	/* clear the color buffer */
+	glClearColor(0.5, 0.5, 0.5, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices_array);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, normals_array);
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0,
+			      texture_coordinates_array);
+	glEnableVertexAttribArray(2);
+
+	ESMatrix modelview;
+	esMatrixLoadIdentity(&modelview);
+	esTranslate(&modelview, 0.0f, 0.0f, -8.0f);
+	esRotate(&modelview, 45.0f, 1.0f, 0.0f, 0.0f);
+	esRotate(&modelview, 45.0f, 0.0f, 1.0f, 0.0f);
+	esRotate(&modelview, 10.0f, 0.0f, 0.0f, 1.0f);
+	esScale(&modelview, 0.475f, 0.475f, 0.475f);
+
+
+	GLfloat aspect = (GLfloat)(height) / (GLfloat)(width);
+
+	ESMatrix projection;
+	esMatrixLoadIdentity(&projection);
+	esFrustum(&projection, -2.8f, +2.8f, -2.8f * aspect, +2.8f * aspect, 6.0f, 10.0f);
+
+	ESMatrix modelviewprojection;
+	esMatrixLoadIdentity(&modelviewprojection);
+	esMatrixMultiply(&modelviewprojection, &modelview, &projection);
+
+	float normal[9];
+	normal[0] = modelview.m[0][0];
+	normal[1] = modelview.m[0][1];
+	normal[2] = modelview.m[0][2];
+	normal[3] = modelview.m[1][0];
+	normal[4] = modelview.m[1][1];
+	normal[5] = modelview.m[1][2];
+	normal[6] = modelview.m[2][0];
+	normal[7] = modelview.m[2][1];
+	normal[8] = modelview.m[2][2];
+
+	glEnable(GL_TEXTURE_2D);
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+		     COMPANION_TEXTURE_WIDTH, COMPANION_TEXTURE_HEIGHT, 0,
+		     GL_RGB, GL_UNSIGNED_BYTE, companion_texture);
+
+	GLint modelviewmatrix_handle = glGetUniformLocation(program, "modelviewMatrix");
+	GLint modelviewprojectionmatrix_handle = glGetUniformLocation(program, "modelviewprojectionMatrix");
+	GLint normalmatrix_handle = glGetUniformLocation(program, "normalMatrix");
+
+	glUniformMatrix4fv(modelviewmatrix_handle, 1, GL_FALSE, &modelview.m[0][0]);
+	glUniformMatrix4fv(modelviewprojectionmatrix_handle, 1, GL_FALSE, &modelviewprojection.m[0][0]);
+	glUniformMatrix3fv(normalmatrix_handle, 1, GL_FALSE, normal);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	GLint texture_loc = glGetUniformLocation(program, "in_texture");
+	glUniform1i(texture_loc, 0); // 0 -> GL_TEXTURE0 in glActiveTexture
+
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+
+	glDrawArrays(GL_TRIANGLES, 0, COMPANION_ARRAY_COUNT);
+
+	glFlush();
+
+#if ONSCREEN
+	eglSwapBuffers(display, surface);
+#endif
+
+	usleep(1000000);
+
+	fflush(stdout);
+
+	return 0;
+}
